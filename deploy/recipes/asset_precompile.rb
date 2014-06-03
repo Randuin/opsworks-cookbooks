80eb2bb0af83ad7895f2bccdd72024114bc22fd3
Robin Liao <randuin@gmail.com>
2014-06-03 15:18:05 -0700

    Handles paths more correctly

--- deploy/recipes/asset_precompile.rb
+++ deploy/recipes/asset_precompile.rb
@@ -1,6 +1,18 @@
-execute 'rake assets:precompile' do
-  cwd "#{node[:deploy_to]}/current"
-  user 'root'
-  command 'bundle exec rake assets:precompile'
-  environment 'RAILS_ENV' => 'production'
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'rails'
+    Chef::Log.debug("Skipping deploy::rails-restart application #{application} as it is not a Rails app")
+    next
+  end
+
+  Chef::Log.debug("Pre compiling assets for #{application}")
+  execute 'rake assets:precompile' do
+    cwd deploy[:current_path]
+    user 'root'
+    command 'bundle exec rake assets:precompile'
+    environment 'RAILS_ENV' => 'production'
+
+    only_if do
+      File.exists?(deploy[:current_path])
+    end
+  end
 end
