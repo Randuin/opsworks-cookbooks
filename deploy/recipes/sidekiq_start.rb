196acb0a11916b86c324c1d283ab9165bdb79991
Robin Liao <robin@trinetcloud.com>
2014-07-21 15:35:14 -0700

    sidekiq configs for opsworks

---
+++ deploy/recipes/sidekiq_start.rb
@@ -0,0 +1,30 @@
+node[:deploy].each do |application, deploy|
+  if deploy['sidekiq']
+    release_path = ::File.join(deploy[:deploy_to], 'current')
+    sidekiq_env = deploy['sidekiq']['rails_env'] || 'production'
+    require_path = ::File.expand_path(deploy['sidekiq']['require'] || '.', release_path)
+
+    template "setup sidekiq.conf" do
+      path "/etc/init/sidekiq-#{application}.conf"
+      source "sidekiq.conf.erb"
+      owner "deploy"
+      group "apache"
+      mode 0644
+      variables({
+        release_path: release_path,
+        require_path: require_path,
+        sidekiq_env: sidekiq_env
+      })
+    end
+
+    service "sidekiq-#{application}" do
+      provider Chef::Provider::Service::Upstart
+      supports stop: true, start: true, restart: true, status: true
+    end
+
+    bash 'restart_sidekiq' do
+      code "echo noop"
+      notifies :restart, "service[sidekiq-#{application}]"
+    end
+  end
+end
