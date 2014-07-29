08f55d22790e9a1a063ee1f47c067280018ce04d
Robin Liao <randuin@gmail.com>
2014-07-28 17:21:56 -0700

    generate whenever crontab

---
+++ deploy/recipes/whenever.rb
@@ -0,0 +1,9 @@
+node[:deploy].each do |application, deploy|
+  bash "update-crontab-#{application}" do
+    cwd deploy[:current_path]
+
+    user 'deploy'
+    command "bundle exec whenever --set environment=#{deploy[:rails_env]} --update-crontab #{application}}"
+    only_if "cd #{deploy[:current_path]} && bundle show whenever"
+  end
+end
